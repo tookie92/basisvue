f18df4c62cb28714711e4914a102f73b44bdaa47
Joseph Ikinda <josephikinda@gmail.com>
2020-03-15 10:35:30 +0100

    je reviens

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -18,14 +18,38 @@ document.addEventListener('turbolinks:load', () => {
       data: function() {
         return { client: client }
       },
+
+
+
       methods: {
-        saveUser() {
+
+
+
+        saveUser: function() {
+          //create a new client
+
+          if (this.id == null) {
           this.$http.post('/clients', { client: this.client }).then(response => {
             Turbolinks.visit(`/clients/${response.body.id}`)
           }, response => {
             console.log(response)
           })
+
+          //edit a client
+        } else{
+          this.$http.put(`/clients/${this.id}.json/edit`, { client: this.client }).then(response => {
+            Turbolinks.visit(`/clients/${response.body.id}`)
+          }, response => {
+            console.log(response)
+          })
         }
+      },
+
+      // End
+
+
+
+
       }
     });
   }
